08b0175b6776abbedc20afdc6c71db375301e4e5
Allan Prieb <allanprieb01@gmail.com>
2024-03-12 22:04:41 -0400

    Added labels on map and bluetooth - [x] Added the annotation fro new fires - [x] Added bluetooth capabilities

--- Sireen/Login/LoginViewController.swift
+++ Sireen/Login/LoginViewController.swift
@@ -61,33 +61,6 @@ class LoginViewController: UIViewController {
         }
     }
     
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        let textTag = textField.tag
-//        let nextResponder = textField.superview?.superview?.viewWithTag(textTag)
-//        if(nextResponder != nil) {
-//            nextResponder?.becomeFirstResponder()
-//        } else {
-//            self.view.endEditing(true)
-//        }
-//    }
-//    
-//    extension UITextField {
-//        
-//        func addDoneToolbar(onDone: (target: Any, action: Selector)? = nil) {
-//            let onDone = onDone ?? (target: self, action: #selector(doneButtonTapped))
-//            
-//            let toolbar: UIToolbar = UIToolbar()
-//            toolbar.barStyle = .default
-//            toolbar.items = {
-//                UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
-//                UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil),
-//                UIBarButtonItem(title: "Done", style: .done, target: onDone.target, action: onDone.action)
-//            }
-//            toolbar.sizeToFit()
-//            self.inputAccessoryView = toolbar
-//             
-//        }
-//    }
 
     private func showMissingFieldsAlert() {
         let alertController = UIAlertController(title: "Opps...", message: "We need all fields filled out in order to log you in.", preferredStyle: .alert)
--- Sireen/MapViewController.swift
+++ Sireen/MapViewController.swift
@@ -1,16 +1,10 @@
-//
-//  MapViewController.swift
-//  Sireen
-//
-//  Created by Allan Prieb on 03/03/24.
-//
-
 import UIKit
 import MapKit
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
     
     let circleFlag = false
+    var isInitialLocationSet = false // Add this property
 
     @IBOutlet weak var mapView: MKMapView!
     
@@ -20,6 +14,7 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
         super.viewDidLoad()
         
         mapView.delegate = self
+        mapView.showsUserLocation = true
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
@@ -36,21 +31,23 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
-            guard let circleOverlay = overlay as? MKCircle else { return MKOverlayRenderer() }
-            
-            let circleRenderer = MKCircleRenderer(circle: circleOverlay)
-            circleRenderer.strokeColor = .blue
-            circleRenderer.fillColor = .blue.withAlphaComponent(0.1)
-            return circleRenderer
-        }
+        guard let circleOverlay = overlay as? MKCircle else { return MKOverlayRenderer() }
+        
+        let circleRenderer = MKCircleRenderer(circle: circleOverlay)
+        circleRenderer.strokeColor = .blue
+        circleRenderer.fillColor = .blue.withAlphaComponent(0.1)
+        return circleRenderer
+    }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-            guard let latestLocation = locations.first else { return }
-            // Do something with the latest location, like centering the map
-            let center = CLLocationCoordinate2D(latitude: latestLocation.coordinate.latitude, longitude: latestLocation.coordinate.longitude)
-            let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
-            mapView.setRegion(region, animated: true)
-        }
+        guard let latestLocation = locations.first, !isInitialLocationSet else { return }
+        
+        // Center the map on the user's location just once
+        let center = CLLocationCoordinate2D(latitude: latestLocation.coordinate.latitude, longitude: latestLocation.coordinate.longitude)
+        let region = MKCoordinateRegion(center: center, latitudinalMeters: 1000, longitudinalMeters: 1000)
+        mapView.setRegion(region, animated: true)
+        isInitialLocationSet = true // Prevent further updates
+    }
     
     func addPins() {
         let sensorPin = MKPointAnnotation()
@@ -65,35 +62,47 @@ class MapViewController: UIViewController, CLLocationManagerDelegate {
     }
 }
 
-
 extension MapViewController: MKMapViewDelegate {
     func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
-        
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "custom")
-        
-        if annotationView == nil {
-            // CREATE VIEW
-            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "custom")
+        // Check if the annotation is the user's location
+        if annotation is MKUserLocation {
+            // Attempt to dequeue an existing pin view first
+            var pinView = mapView.dequeueReusableAnnotationView(withIdentifier: "UserLocation") as? MKPinAnnotationView
+            
+            if pinView == nil {
+                // If an existing pin view was not available, create one
+                pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: "UserLocation")
+                pinView?.pinTintColor = UIColor.green // Customize pin color
+                pinView?.canShowCallout = true // Optionally show a callout
+            } else {
+                pinView?.annotation = annotation
+            }
+            
+            return pinView
         } else {
-            // Assign annotation
-            annotationView?.annotation = annotation
-        }
-        
-        // set custom annotation images
-        
-        switch annotation.title {
+            // Handle other annotations (fire, sensor)
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: "custom")
+            
+            if annotationView == nil {
+                // CREATE VIEW
+                annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "custom")
+            } else {
+                // Assign annotation
+                annotationView?.annotation = annotation
+            }
+            
+            // Set custom annotation images based on the title
+            switch annotation.title {
             case "fire":
                 annotationView?.image = UIImage(named: "fireWarning")
             case "sensor":
                 annotationView?.image = UIImage(named: "bluePin")
             default:
                 break
+            }
+            
+            return annotationView
         }
-        
-        return annotationView
-        
     }
-    
 }
-
 
